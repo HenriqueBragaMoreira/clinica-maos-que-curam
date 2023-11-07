1fa41794fc0bf2c07378f1dc271999dc9535d1c1
HenriqueBragaMoreira <shenrique40moreira@gmail.com>
2023-11-06 21:33:20 -0300

    [refactored] a lot fixed bugs and UI design

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pt-BR">
+      <title>Quiropraxia Mãos que Curam</title>
       <Head />
       <body>
         <Main />
