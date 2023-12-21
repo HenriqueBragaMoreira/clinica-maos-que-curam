13de8395d446ac2ff2b3005a9518f94e5ee1414b
HenriqueBragaMoreira <shenrique40moreira@gmail.com>
2023-12-20 23:14:23 -0300

    added a function to the offer and improve buttons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,19 @@
 import Image from "next/image";
 import logo from "../../public/logo.svg";
-import { MdOutlineMail } from "react-icons/md";
+import mid from "../../public/mid.jpg";
+import left from "../../public/left.jpg";
+import right from "../../public/right.jpg";
 import { BsInstagram, BsTiktok, BsWhatsapp } from "react-icons/bs";
 
 export default function Home() {
   return (
     <div>
       <div className="mx-5 xl:mx-20">
-        <header className="min-h-screen mb-14 lg:mb-0">
+        <header className="min-h-screen mb-14 lg:mb-0 flex flex-col justify-center">
           <div className="flex justify-around items-center">
             <div className="flex flex-col text-center items-center">
               <h1 className="text-6xl 2xl:text-8xl md:text-6xl xl:text-7xl font-cursive mt-8 lg:mt-0">
-                Quiropraxia Mãos Que Curam
+                Quiropraxia Mãos<br/> Que Curam
               </h1>
               <Image
                 src={logo}
@@ -35,6 +37,7 @@ export default function Home() {
             </div>
 
             <Image
+              priority
               src={logo}
               alt="Picture of the author"
               width={420}
@@ -73,9 +76,9 @@ export default function Home() {
           </div>
 
           <div className="flex justify-around flex-col items-center lg:flex-row sm:items-center gap-7">
-            <div className="bg-gray-500 w-80 h-72 text-white">AA</div>
-            <div className="bg-gray-500 w-80 h-72 text-white">AA</div>
-            <div className="bg-gray-500 w-80 h-72 text-white">AA</div>
+            <Image className="w-[350px] h-96 rounded-lg border-solid border-[4px] border-white" src={left} alt="Foto da sala de quiropraxia"/>
+            <Image className="w-[350px] h-96 rounded-lg border-solid border-[4px] border-white" src={mid} alt="Foto da recepção da clinica"/>
+            <Image className="w-[350px] h-96 rounded-lg border-solid border-[4px] border-white" src={right} alt="Foto da sala de massoterapia"/>
           </div>
         </main>
 
@@ -91,7 +94,7 @@ export default function Home() {
             criar <br /> um plano de tratamento que funcione.
           </p>
           <div className="flex justify-around flex-col items-center lg:flex-row sm:items-center gap-6">
-            <div className="bg-purple-300 w-72 h-72 rounded-3xl pt-4 flex flex-col justify-between items-center py-2 px-4">
+            <div className="bg-purple-300 w-72 h-72 rounded-3xl pt-4 flex flex-col justify-between items-center py-2 px-4 border-solid border-white border-[3px]">
               <p className="text-lg font-bold">Pacote de 5 sessões</p>
               <p className="font-semibold">
                 Desconecte do estresse diário e mime sua coluna com nosso Pacote
@@ -99,10 +102,15 @@ export default function Home() {
                 promova o equilíbrio e renove sua vitalidade.
               </p>
               <button className="bg-gray-500 p-2 text-white rounded-md w-28">
-                Adquira já!
+                <a
+                  target="_blank"
+                  href="https://wa.me/5511914215301?text=Olá, tudo bem? Vim do site de vocês e queria saber mais sobre o pacote de 5 sessões!"
+                >
+                  Adquira já!
+                </a>
               </button>
             </div>
-            <div className="bg-purple-300 w-72 h-72 rounded-3xl pt-4 flex flex-col justify-between items-center py-2 px-4">
+            <div className="bg-purple-300 w-72 h-72 rounded-3xl pt-4 flex flex-col justify-between items-center py-2 px-4 border-solid border-white border-[3px]">
               <p className="text-lg font-bold">Pacote de 10 sessões</p>
               <p className="font-semibold">
                 Invista na sua saúde com nosso Pacote Vitalidade de 10 sessões
@@ -110,10 +118,15 @@ export default function Home() {
                 e fortaleça seu corpo para uma vida mais ativa.
               </p>
               <button className="bg-gray-500 p-2 text-white rounded-md w-28">
-                Adquira já!
+                <a
+                  target="_blank"
+                  href="https://wa.me/5511914215301?text=Olá, tudo bem? Vim do site de vocês e queria saber mais sobre o pacote de 10 sessões!"
+                >
+                  Adquira já!
+                </a>
               </button>
             </div>
-            <div className="bg-purple-300 w-72 h-72 rounded-3xl pt-4 flex flex-col justify-between items-center py-2 px-4">
+            <div className="bg-purple-300 w-72 h-72 rounded-3xl pt-4 flex flex-col justify-between items-center py-2 px-4 border-solid border-white border-[3px]">
               <p className="text-lg font-bold">Pacote de 15 sessões</p>
               <p className="font-semibold">
                 Experimente uma transformação completa com nosso Pacote
@@ -122,10 +135,15 @@ export default function Home() {
                 sua qualidade de vida.
               </p>
               <button className="bg-gray-500 p-2 text-white rounded-md w-28">
-                Adquira já!
+                <a
+                  target="_blank"
+                  href="https://wa.me/5511914215301?text=Olá, tudo bem? Vim do site de vocês e queria saber mais sobre o pacote de 15 sessões!"
+                >
+                  Adquira já!
+                </a>
               </button>
             </div>
-            <div className="bg-purple-300 w-72 h-72 rounded-3xl pt-4 flex flex-col justify-between items-center py-2 px-4">
+            <div className="bg-purple-300 w-72 h-72 rounded-3xl pt-4 flex flex-col justify-between items-center py-2 px-4 border-solid border-white border-[3px]">
               <p className="text-lg font-bold">Pacote de 20 sessões</p>
               <p className="font-semibold">
                 Cuide do seu corpo a longo prazo com o Pacote Total de 20
@@ -134,7 +152,12 @@ export default function Home() {
                 incomparável.
               </p>
               <button className="bg-gray-500 p-2 text-white rounded-md w-28">
-                Adquira já!
+                <a
+                  target="_blank"
+                  href="https://wa.me/5511914215301?text=Olá, tudo bem? Vim do site de vocês e queria saber mais sobre o pacote de 20 sessões!"
+                >
+                  Adquira já!
+                </a>
               </button>
             </div>
           </div>
@@ -144,8 +167,8 @@ export default function Home() {
           <h1 className="text-5xl font-semibold 2xl:text-8xl md:text-6xl xl:text-7xl mb-12 text-center">
             Onde Ficamos Localizados
           </h1>
-          <div className="lg:flex lg:flex-row justify-around h-[500px]">
-            <div className="mt-20 bg-purple-300 w-auto h-96 rounded-3xl ">
+          <div className="lg:flex lg:flex-row justify-around">
+            <div className="mt-24 bg-purple-300 w-auto h-80 rounded-3xl border-solid border-white border-[3px]">
               <p className="font-bold text-2xl text-center pt-4">Contato</p>
               <div className="flex flex-col gap-8 mt-12 ml-8 mr-8">
                 <div className="flex items-center gap-4">
@@ -160,23 +183,20 @@ export default function Home() {
                   <BsTiktok className="text-4xl" />
                   <p className="text-lg font-bold">@quiro.maosquecuram</p>
                 </div>
-                <div className="flex items-center gap-4">
-                  <MdOutlineMail className="text-4xl" />
-                  <p className="text-lg font-bold"></p>
-                </div>
               </div>
             </div>
             <div className="flex items-center">
               <iframe
+                title="Mapa com a localização da clinica interativo"
                 className="mt-10 rounded-md border-solid border-4 border-purple-300 w-600 h-450"
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d117037.17676670375!2d-46.63592618531937!3d-23.55365014938566!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5f7198cb22a3%3A0x72acd74cad4440d1!2sQuiropraxia%20M%C3%A3os%20que%20Curam!5e0!3m2!1spt-BR!2sbr!4v1702688176993!5m2!1spt-BR!2sbr"
                 loading="lazy"
-              ></iframe>
+              />
             </div>
           </div>
         </div>
       </div>
-      <footer className="p-1.5 mt-[490px] md:mt-auto sm:mt-[490px] lg:mt-auto bg-black w-full text-white text-center">
+      <footer className="p-1.5 mt-[490px] md:mt-[490px] sm:mt-[490px] lg:mt-auto bg-black w-full text-white text-center">
         Desenvolved by{" "}
         <a
           href="https://github.com/HenriqueBragaMoreira"
