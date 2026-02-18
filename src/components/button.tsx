7574013653c47e0e6b66978e7e36a4350bdd9b1d
Henrique Braga <shenrique40moreira@gmail.com>
2026-02-17 23:25:07 -0300

    refactor(button): enhance Button component with className prop for flexible styling
    - Add className prop to Button component for custom styling
    - Utilize utility function `cn` for merging default and custom classes

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,10 +1,20 @@
+import { cn } from "@/lib/cn";
 import { ArrowRight } from "lucide-react";
 
-export function Button({ children }: { children: React.ReactNode }) {
+export function Button({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
   return (
     <button
       type="button"
-      className="group flex items-center cursor-pointer gap-1.5 bg-button-primary text-white pl-4 pr-1.5 py-1.5 rounded-full font-medium font-satoshi shadow-[0px_0px_38px_0px_rgba(156,99,73,0.28)]"
+      className={cn(
+        "group flex items-center cursor-pointer gap-1.5 bg-button-primary text-white pl-4 pr-1.5 py-1.5 rounded-full font-medium font-satoshi shadow-[0px_0px_38px_0px_rgba(156,99,73,0.28)]",
+        className,
+      )}
     >
       <span className="-mt-0.5">{children}</span>
 
